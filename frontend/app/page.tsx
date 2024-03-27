888ee848add18bc8b365d6961c7a93142338aec9
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-03-27 19:57:31 +0500

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,12 +4,11 @@ import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
-import Image from "next/image";
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
   const router = useRouter();
 
@@ -26,7 +25,7 @@ const Login = () => {
           },
           body: JSON.stringify({
             email,
-            password,
+            name,
           }),
         }
       );
@@ -35,7 +34,7 @@ const Login = () => {
         throw new Error("Email is required");
       }
 
-      if (!password) {
+      if (!name) {
         throw new Error("Password is required");
       }
 
@@ -45,7 +44,7 @@ const Login = () => {
 
         console.log(data);
       } else {
-        toast.error("Invalid email or password");
+        toast.error("Invalid name or password");
       }
     } catch (error: any) {
       toast.error(`${error.message}`);
@@ -67,6 +66,16 @@ const Login = () => {
 
           <div className="md:w-72 lg:w-80 mx-auto">
             <div className="mb-4">
+              <label className="font-bold text-white">Name</label>
+              <input
+                className="w-full px-3 py-2 border bg-gray-100 text-black rounded-lg placeholder-gray-900 focus:outline-none border-gray-400"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+              />
+            </div>
+            <div className="mb-2">
               <label className="font-bold text-white">Email</label>
               <input
                 className="w-full px-3 py-2 border bg-gray-100 text-black rounded-lg placeholder-gray-900 focus:outline-none border-gray-400"
@@ -76,32 +85,10 @@ const Login = () => {
                 placeholder="Email"
               />
             </div>
-            <div className="mb-2">
-              <label className="font-bold text-white">Password</label>
-              <input
-                className="w-full px-3 py-2 border bg-gray-100 text-black rounded-lg placeholder-gray-900 focus:outline-none border-gray-400"
-                type="password"
-                placeholder="Password"
-                value={password} // Removed brackets from here
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="flex items-center justify-between mb-8">
-              <label className="flex items-center text-gray-600">
-                <input type="checkbox" className="mr-2" />
-                <p className="text-sm text-white">Keep me signed in</p>
-              </label>
-              <Link
-                href="/forgot-pass"
-                className="text-sm font-semibold text-white underline tracking-wider"
-              >
-                Forgot password
-              </Link>
-            </div>
             <button
               disabled={loading}
               onClick={handleLogin}
-              className="w-full px-3 py-2 border font-semibold text-gray-100 bg-black rounded-lg focus:outline-none focus:border-gray-900 hover:bg-gray-900"
+              className="w-full px-3 mt-8 py-2 border font-semibold text-gray-100 bg-black rounded-lg focus:outline-none focus:border-gray-900 hover:bg-gray-900"
             >
               Sign in
             </button>
